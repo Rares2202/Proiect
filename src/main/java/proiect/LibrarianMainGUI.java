653a571472333056cd242f0284ac1b6c081ee647
Ilasoaia-George <201130791+GeoILS@users.noreply.github.com>
2025-04-03 21:17:48 +0300

    Interfata main (FXML, CSS, FontAwsomeFX)

--- src/main/java/proiect/LibrarianMainGUI.java
+++ src/main/java/proiect/LibrarianMainGUI.java
@@ -1,28 +1,45 @@
 package proiect;
 
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
 public class LibrarianMainGUI extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
 
     @Override
-    public void start(Stage stage) throws IOException {
+    public void start(Stage mainStage) throws IOException {
 
-        //load fxml files
-        Parent root = FXMLLoader.load(getClass().getResource("fxml/LibrarianMain.fxml"));
+        //load fxml file
+        Parent window = FXMLLoader.load(getClass().getResource("fxml/LibrarianMain.fxml"));
 
-        //create GUIs
-        Scene mainGUI = new Scene(root);
+        //create GUI
+        Scene scene = new Scene(window);
+        scene.getStylesheets().add(this.getClass().getResource("css/style.css").toExternalForm());
 
-        //set main GUI
-        stage.setTitle("LibraryApp");
-        stage.setScene(mainGUI);
-        stage.show();
+        //enable window moveing on screen
+        window.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+
+        window.setOnMouseDragged(event -> {
+
+            mainStage.setX(event.getScreenX() - xOffset);
+            mainStage.setY(event.getScreenY() - yOffset);
+        });
+
+        //display app
+        mainStage.initStyle(StageStyle.UNDECORATED);
+        mainStage.setScene(scene);
+        mainStage.show();
     }
 
     public static void main(String[] args) {
