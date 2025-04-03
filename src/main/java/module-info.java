2271061d1fc7817d90173ed3c299f022e1656484
Ilasoaia-George <201130791+GeoILS@users.noreply.github.com>
2025-04-03 21:17:48 +0300

    Interfata main (FXML, CSS, FontAwsomeFX)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,9 @@ module Biblioteca {
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.web;
+    requires java.desktop;
+    requires java.sql;
+    requires fontawesomefx;
 
     opens proiect to javafx.fxml;
     exports proiect;
---
+++ src/main/java/proiect/ControllerLibrarian.java
@@ -0,0 +1,23 @@
+package proiect;
+
+import javafx.animation.FadeTransition;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.util.Duration;
+
+import java.io.IOException;
+
+
+public class ControllerLibrarian {
+
+
+    public void quit_app(MouseEvent mouseEvent) {
+        System.exit(0);
+    }
+}
