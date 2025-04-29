bb68cd8434f59563ee1b0f7714f460af129fd369
Rares2202 <rares-gabriel.stiubei@student.tuiasi.ro>
2025-04-29 18:32:42 +0300

    Update ControllerMain.java

--- src/main/java/proiect/controller/ControllerMain.java
+++ src/main/java/proiect/controller/ControllerMain.java
@@ -21,6 +21,7 @@ public class ControllerMain {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/mydb";
     private static final String DB_USER = "root";
     private static final String DB_PASSWORD = "simone";
+    private static final String DB_PASSWORD = "root";
     private final String[] buttonIds = {
             "register", "login", "inchide", "submit", "register1","login1"
     };
